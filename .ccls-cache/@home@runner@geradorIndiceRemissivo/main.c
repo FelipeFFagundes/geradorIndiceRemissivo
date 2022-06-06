429fc1f279266d4d9a01a8186cb3413c4d4179d1
Felipe Fonseca Fagundes <71851617+FelipeFFagundes@users.noreply.github.com>
2022-06-06 16:33:28 +0000

    contador de palavras simples implementado

--- .ccls-cache/@home@runner@geradorIndiceRemissivo/main.c
+++ .ccls-cache/@home@runner@geradorIndiceRemissivo/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int main(void) {
-  printf("testando trabalho\n");
-  return 0;
+char frase[50];
+int i, flag=0, total=0;
+
+printf("Insira a frase:\n");
+fgets(frase,51,stdin);
+
+    for(i=0;frase[i]!='\0';i++) {
+    	if(!flag && frase[i]!=' ') {
+    		flag=1;
+    		total++;
+    	}
+    	else if(flag && frase[i]==' ')
+    		flag=0;
+    }
+    
+printf("numero total de palavras: %d\n",total);
+
+return 0;
 }
