88176625cb9f0a3ad97a279dd8f92d603c9ac21a
Felipe Fonseca Fagundes <71851617+FelipeFFagundes@users.noreply.github.com>
2022-06-08 17:14:13 +0000

    testando diferentes alternativas de ler e contar

--- remissivoV3.c
+++ remissivoV3.c
@@ -1,33 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define MAX 30
+ 
+int ocorrencias(char palavra[], char letra, int i) {
+ int contagem = 0;
 
-
-int main(){
-  char ch, variavel[]="Tem", file_name[25];
-  int count=0;
-  FILE *fp;
-
-  printf("Por favor digite o nome do arquivo\n");
-  gets(file_name);
-  printf("\n");
-  
-  fp = fopen(file_name, "r");
-  printf("\n");
-
-  if (fp == NULL){
-    perror("Erro ao abrir o arquivo.\n");
-    exit(EXIT_FAILURE);
-  }
-
-  printf("Os conteúdo do arquivo %s são:\n", file_name);
-
-  while((ch = fgetc(fp)) != EOF){
-    if(ch==variavel){
-        count++;
-    }
-    printf("%c", ch);
+    while (palavra[i] != '\0') contagem += letra == palavra[i++];
+    return contagem;
 }
-  printf("%d",count);
-  fclose(fp);
-  return 0;
+ 
+int main() {
+	char palavra[MAX];
+	char letra;
+	scanf("%s %c", palavra, &letra);
+	printf("%d", ocorrencias(palavra, letra, 0));
 }
